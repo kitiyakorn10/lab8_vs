b8f3c34856b209717e1a9164b61f85d65de48226
kitiyakorn10 <62237303+kitiyakorn10@users.noreply.github.com>
2020-11-12 15:06:47 +0700

    Create sum.test.js

---
+++ lab8_vs/sum.test.js
@@ -0,0 +1,14 @@
+  
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+    //Arrange
+    let a = 1
+    let b = 2
+
+    //Act
+    let result = sum(a,b)
+
+//Assert
+ expect(3).toBe(result);
+});
